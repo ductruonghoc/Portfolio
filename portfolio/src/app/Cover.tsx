148d507fa6437536c43312c22a9806c0f881bec1
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2024-09-07 18:06:30 +0700

    1.0
    Implement, Cover and Avatar

---
+++ portfolio/src/app/Cover.tsx
@@ -0,0 +1,11 @@
+export default function Cover() {
+  return (
+    <div
+      className="container bg-amber-100 border-solid border-2 border-amber-300 ring-offset-2 ring ring-amber-50 rounded-md p-3"
+    >
+      <div
+        className="border-dashed border-2 border-yellow-200 h-48 w-48 bg-[url('img/8284224.jpg')] bg-[length:36rem_26rem] bg-left-bottom hover:bg-top">
+        </div>
+    </div>
+  );
+}
